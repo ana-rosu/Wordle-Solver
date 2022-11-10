9202a3a5a5a32d3bd1ec522519fa25f0a920b805
Ana Roșu <anarosu766@gmail.com>
2022-11-11 00:37:00 +0200

    list version

--- teste2.py
+++ teste2.py
@@ -1,25 +1,47 @@
-import shutil
+from scipy.stats import entropy
+import math
+permutari_fisier = open('permutari.txt')
+permutari_lista = permutari_fisier.read().split()
+cuvinte = open('cuvinte_wordle.txt').read().split() #lista
+cuvinte2 = cuvinte.copy()
+def remove_word(lista,ch):
+    for el in list(lista):
+        if ch in el:
+            lista.remove(el)
+def entropy1(lista):
+    # l = [(el * (-math.log2(el)) for el in lista)]
+    l=[]
+    for el in lista:
+        l.append(el * (-math.log2(el)))
+    print(l)
+    return sum(l)
 
-permutari = open('permutari.txt')
-def remove_word(file,cuv,i):
-    for line in file:
-        if cuv[i] in line:
-            yield line
 def parcurgere():
-    while open('cuvinte_wordle.txt').read():
-        cuvinte = open('cuvinte_wordle.txt').read().split()
+   # while open('cuvinte_wordle.txt').read():
+        max=0
+        cuvinte = open('cuvinte_wordle.txt').read().split() #lista
         for cuv in cuvinte:
-            copie = open('copie_cuvinte.txt', 'r+')
-            copie.truncate()
-            shutil.copyfile('cuvinte_wordle.txt', 'Wordle-Game/copie_cuvinte.txt')
-            for cod in permutari.read().split():
-                for i,cifra in enumerate(str(cod)):
-                    if cifra == 0:
-                        remove_word(copie,cuv,i)
-                        print('a')
-        break
+      #      copie = open('copie_cuvinte.txt').read().split() #lista
+          #  copie = []
+          #   copie = cuvinte.copy()
+            prob = []
+            for cod in permutari_lista:
+                copie = cuvinte.copy()
+                n = len(copie)
+                for i, cifra in enumerate(str(cod)):
+                    if cifra == '0':
+                        remove_word(copie, cuv[i])
+                ramase = len(copie)
+                prob.append(ramase/n)
+            print(entropy(prob, base=2))
+            if entropy(prob, base=2) > max:
+                max = entropy(prob, base=2)
+                guess = cuv
+
+        print(guess)
 
 parcurgere()
+
 
 
 
